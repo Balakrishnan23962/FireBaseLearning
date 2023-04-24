a5569ec17e3c10dcac2fd03136cf8b10417d8425
Balakrishnan23962 <balakrishnan5523962@gmail.com>
2023-04-24 13:57:00 +0530

    Initial Commit

---
+++ FireBaseLearning/FireBaseLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  FireBaseLearningApp.swift
+//  FireBaseLearning
+//
+//  Created by Gasc_Internship1 on 24/04/23.
+//
+
+import SwiftUI
+
+@main
+struct FireBaseLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
